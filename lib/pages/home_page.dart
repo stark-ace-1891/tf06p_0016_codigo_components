c87c9a7507dfb9273551be3f402ca963ed4f67bb
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-08 23:37:49 -0500

    Extrayendo widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -44,41 +44,55 @@ class HomePage extends StatelessWidget {
             width: 160,
             child: Divider(),
           ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.07),
-                  blurRadius: 12,
-                  offset: Offset(4, 4),
-                )
-              ],
-            ),
-            child: ListTile(
-              leading: Icon(
-                Icons.check_circle_outline,
-                color: Color(0xff14213d),
-              ),
-              title: Text(
-                "Avatar",
-                style: GoogleFonts.poppins(),
-              ),
-              subtitle: Text(
-                "Ir al detalle del avatar",
-                style: GoogleFonts.poppins(),
-              ),
-              trailing: Icon(
-                Icons.chevron_right,
-                color: Color(0xff14213d),
-              ),
-            ),
-          ),
-        
+          ItemComponentWidget(),
+          ItemComponentWidget(),
+          ItemComponentWidget(),
+          ItemComponentWidget(),
+          ItemComponentWidget(),
         ],
       ),
     ));
+  }
+}
+
+class ItemComponentWidget extends StatelessWidget {
+  const ItemComponentWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 7),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.07),
+            blurRadius: 12,
+            offset: Offset(4, 4),
+          )
+        ],
+      ),
+      child: ListTile(
+        leading: Icon(
+          Icons.check_circle_outline,
+          color: Color(0xff14213d),
+        ),
+        title: Text(
+          "Avatar",
+          style: GoogleFonts.poppins(),
+        ),
+        subtitle: Text(
+          "Ir al detalle del avatar",
+          style: GoogleFonts.poppins(),
+        ),
+        trailing: Icon(
+          Icons.chevron_right,
+          color: Color(0xff14213d),
+        ),
+      ),
+    );
   }
 }
