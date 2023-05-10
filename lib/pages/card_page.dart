b1cbb8c6596187b52f00723dfed693e42101d4ad
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-10 13:57:28 -0500

    Agregando boton al card page

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -29,11 +29,48 @@ class CardPage extends StatelessWidget {
                 ),
               ],
             ),
-            child: Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              maxLines: 5,
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
+            child: Column(
+              children: [
+                Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                  maxLines: 5,
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.66),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 12,
+                  ),
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  // height: 40,
+                  child: Text(
+                    "Follow me",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: BorderRadius.circular(40),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.deepPurpleAccent.withOpacity(0.6),
+                        blurRadius: 10,
+                        offset: Offset(4,4),
+                      )
+                    ],
+                  ),
+                ),
+              ],
             ),
           )
         ],
