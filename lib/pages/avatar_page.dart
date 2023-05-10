6cf780495b526d31f81b16835f07a159536ca245
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-10 01:00:10 -0500

    Agregando appbar e imagen al widget avatar

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -8,6 +8,36 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.deepPurple,
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+              "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          FadeInImage(
+            image: NetworkImage(
+                "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+            placeholder: AssetImage(
+              "assets/images/loading.gif",
+            ),
+            height: 300,
+            fit: BoxFit.fitWidth,
+            fadeInDuration: Duration(
+              milliseconds: 1300,
+            ),
+          ),
+        ],
       ),
     );
   }
