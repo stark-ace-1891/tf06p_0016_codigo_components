2b4b91c5b25f3e8a5a313d67eb7f893f95a67cf7
Cesar Morales <cesar.aml1891@gmail.com>
2023-05-10 00:08:14 -0500

    Agregando SingleChildScrollView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,53 +5,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-              // color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle.jpg"),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  offset: Offset(10, 10),
-                  blurRadius: 12,
+              Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                  // color: Colors.redAccent,
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        "https://malditopaparazzo.com.ar/wp-content/uploads/2021/05/David-Chicle.jpg"),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      offset: Offset(10, 10),
+                      blurRadius: 12,
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Flutter Components",
+                style: GoogleFonts.poppins(
+                  fontSize: 22,
+                  letterSpacing: 1,
+                ), //Para que funciona se requiere internet
+              ),
+              const SizedBox(
+                width: 160,
+                child: Divider(),
+              ),
+              ItemComponentWidget(),
+            ],
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            "Flutter Components",
-            style: GoogleFonts.poppins(
-              fontSize: 22,
-              letterSpacing: 1,
-            ), //Para que funciona se requiere internet
-          ),
-          const SizedBox(
-            width: 160,
-            child: Divider(),
-          ),
-          ItemComponentWidget(),
-          ItemComponentWidget(),
-          ItemComponentWidget(),
-          ItemComponentWidget(),
-          ItemComponentWidget(),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
 
