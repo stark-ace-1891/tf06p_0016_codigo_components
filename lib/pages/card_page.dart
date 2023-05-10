3d9951fe4cedb32bde758a762871fb9956729fc9
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-10 14:28:07 -0500

    Agregando 2 card con row y aplicando decoraciones

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -10,6 +10,7 @@ class CardPage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          //Card 1
           Container(
             margin: EdgeInsets.all(20),
             padding: EdgeInsets.all(12),
@@ -65,11 +66,66 @@ class CardPage extends StatelessWidget {
                       BoxShadow(
                         color: Colors.deepPurpleAccent.withOpacity(0.6),
                         blurRadius: 10,
-                        offset: Offset(4,4),
+                        offset: Offset(4, 4),
                       )
                     ],
                   ),
                 ),
+              ],
+            ),
+          ),
+
+          //Card 2
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 12, horizontal: 14),
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(14),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.06),
+                  offset: Offset(4, 4),
+                  blurRadius: 12,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Image.asset(
+                  "assets/images/imagex1.png",
+                  height: 120,
+                ),
+                SizedBox(
+                  width: 4,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Fiorella de las Nieves Azules",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 4,
+                        style: TextStyle(
+                          color: Colors.black54,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           )
