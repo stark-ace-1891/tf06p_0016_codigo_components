e388d1436de63fff57cc6e50acb1fc3d24ba3032
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-11 18:22:49 -0500

    Primer reto

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -8,184 +8,292 @@ class CardPage extends StatelessWidget {
         backgroundColor: Colors.redAccent,
         title: Text("Card Page"),
       ),
-      body: Column(
-        children: [
-          //Card 1
-          Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(18),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.07),
-                  offset: Offset(4, 4),
-                  blurRadius: 12,
-                ),
-                BoxShadow(
-                  color: Colors.white.withOpacity(0.07),
-                  offset: Offset(-5, -5),
-                  blurRadius: 6,
-                ),
-              ],
-            ),
-            child: Column(
-              children: [
-                Text(
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                  maxLines: 5,
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.66),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //Card 1
+            Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.07),
+                    offset: Offset(4, 4),
+                    blurRadius: 12,
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
+                  BoxShadow(
+                    color: Colors.white.withOpacity(0.07),
+                    offset: Offset(-5, -5),
+                    blurRadius: 6,
                   ),
-                  padding: EdgeInsets.symmetric(
-                    vertical: 12,
-                  ),
-                  alignment: Alignment.center,
-                  width: double.infinity,
-                  // height: 40,
-                  child: Text(
-                    "Follow me",
+                ],
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                    maxLines: 5,
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
                     style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
+                      color: Colors.black.withOpacity(0.66),
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    color: Colors.deepPurpleAccent,
-                    borderRadius: BorderRadius.circular(40),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.deepPurpleAccent.withOpacity(0.6),
-                        blurRadius: 10,
-                        offset: Offset(4, 4),
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 12,
+                    ),
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    // height: 40,
+                    child: Text(
+                      "Follow me",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
                       ),
-                    ],
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurpleAccent,
+                      borderRadius: BorderRadius.circular(40),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.deepPurpleAccent.withOpacity(0.6),
+                          blurRadius: 10,
+                          offset: Offset(4, 4),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          //Card 2
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 12, horizontal: 14),
-            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(14),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.06),
-                  offset: Offset(4, 4),
-                  blurRadius: 12,
-                ),
-              ],
+            //Card 2
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 12, horizontal: 14),
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(14),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    offset: Offset(4, 4),
+                    blurRadius: 12,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/images/imagex1.png",
+                    height: 120,
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Fiorella de las Nieves Azules",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 4,
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              children: [
-                Image.asset(
-                  "assets/images/imagex1.png",
-                  height: 120,
-                ),
-                SizedBox(
-                  width: 4,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Fiorella de las Nieves Azules",
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                            color: Colors.black87,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 4,
-                      ),
-                      Text(
+
+            //Card 3
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 14,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    blurRadius: 12,
+                    offset: Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Text(
                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 4,
-                        style: TextStyle(
-                          color: Colors.black54,
+                        maxLines: 6,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 130,
+                    height: 130,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://images.pexels.com/photos/16763929/pexels-photo-16763929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          //Card 3
-          Container(
-            margin: EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 14,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.06),
-                  blurRadius: 12,
-                  offset: Offset(4, 4),
-                ),
-              ],
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(6.0),
-                    child: Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                      maxLines: 6,
                     ),
                   ),
-                ),
-                Container(
-                  width: 130,
-                  height: 130,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                        "https://images.pexels.com/photos/16763929/pexels-photo-16763929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                ],
+              ),
+            ),
+
+            //Card Reto
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 12, horizontal: 14),
+              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(14),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    offset: Offset(4, 4),
+                    blurRadius: 12,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          "https://images.pexels.com/photos/16763929/pexels-photo-16763929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+                        ),
                       ),
                     ),
                   ),
-                ),
-
-                // ClipRRect(
-                //   borderRadius: BorderRadius.circular(16),
-                //   child: Image.network(
-                //     "https://images.pexels.com/photos/16763929/pexels-photo-16763929.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-                //     height: 130,
-                //     width: 130,
-                //     fit: BoxFit.cover,
-                //   ),
-                // ),
-              ],
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "John Doe",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "CEO at Apple Inc",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: Colors.blue.withOpacity(0.18),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.settings,
+                          size: 20,
+                          color: Colors.blue.withOpacity(0.8),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          "Settings",
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.blue.withOpacity(0.8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        
-        ],
+
+            // Container(
+            //   child: Row(
+            //     children: [
+            //       CircleAvatar(),
+            //       Column(
+            //         children: [
+            //           Text("xxxx"),
+            //           Text("yyy"),
+            //         ],
+            //       )
+            //     ],
+            //   ),
+            // ),
+
+            // Container(
+            //   child: Column(
+            //     children: [
+            //       Row(
+            //         children: [
+            //           Text("xxx"),
+            //           Text("xxx"),
+            //           Container(
+            //             child: Text("xxx"),
+            //           )
+            //         ],
+            //       )
+            //     ],
+            //   ),
+            // ),
+          ],
+        ),
       ),
     );
   }
