c5f012ef996b3ca8562e6d9b693ef37b50944288
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-12 14:06:18 -0500

    Controlando on tap, on chnaged y on submit del teclado

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -17,126 +17,152 @@ class _InputPageState extends State<InputPage> {
           "Input text",
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              style: TextStyle(
-                color: Colors.red,
-              ),
-              maxLines: 1,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                hintText: "Direccion",
-                hintStyle: TextStyle(
-                  color: Colors.blue,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                style: TextStyle(
+                  color: Colors.red,
                 ),
-                label: Text("Direccion Label"),
-                icon: Icon(
-                  Icons.location_on,
-                ),
-                suffix: Icon(
-                  Icons.check_circle_outline,
-                ),
-                prefix: Icon(
-                  Icons.star,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 14,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Buscar producto",
-                prefixIcon: Icon(
-                  Icons.search,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.deepPurpleAccent,
-                    width: 3,
-                  ),
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  // borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    color: Colors.deepPurpleAccent,
-                    width: 3,
-                  ),
-                ),
-              ),
-              onChanged: (String value) {
-                print(value);
-              },
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              decoration: BoxDecoration(boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.06),
-                  offset: Offset(3, 4),
-                  blurRadius: 12,
-                ),
-              ]),
-              child: TextField(
+                maxLines: 1,
+                textAlign: TextAlign.center,
                 decoration: InputDecoration(
-                  hintText: "Buscar producto...",
-                  hintStyle: GoogleFonts.poppins(
-                    fontSize: 14,
-                    color: Colors.black38,
+                  hintText: "Direccion",
+                  hintStyle: TextStyle(
+                    color: Colors.blue,
                   ),
-                  fillColor: Colors.white,
-                  filled: true,
-                  suffixIcon: Container(
-                    margin: EdgeInsets.all(2.5),
-                    decoration: BoxDecoration(
-                      color: Colors.pinkAccent,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
+                  label: Text("Direccion Label"),
+                  icon: Icon(
+                    Icons.location_on,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 0,
-                    ),
+                  suffix: Icon(
+                    Icons.check_circle_outline,
+                  ),
+                  prefix: Icon(
+                    Icons.star,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Buscar producto",
+                  prefixIcon: Icon(
+                    Icons.search,
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(14),
+                    borderRadius: BorderRadius.circular(12),
                     borderSide: BorderSide(
-                      color: Colors.white,
-                      width: 0,
+                      color: Colors.deepPurpleAccent,
+                      width: 3,
+                    ),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    // borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(
+                      color: Colors.deepPurpleAccent,
+                      width: 3,
+                    ),
+                  ),
+                ),
+                onChanged: (String value) {
+                  print(value);
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.06),
+                    offset: Offset(3, 4),
+                    blurRadius: 12,
+                  ),
+                ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Buscar producto...",
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 14,
+                      color: Colors.black38,
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon: Container(
+                      margin: EdgeInsets.all(2.5),
+                      decoration: BoxDecoration(
+                        color: Colors.pinkAccent,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            TextField(
-              obscureText: isInvisible,
-              // obscuringCharacter: "x",
-              decoration: InputDecoration(
-                hintText: "Contraseña",
-                suffix: IconButton(
-                  onPressed: () {
-                    isInvisible = !isInvisible;
-                    setState(() {
-                      
-                    });
-                  },
-                  icon: isInvisible ? Icon(Icons.remove_red_eye) : Icon(Icons.remove_red_eye_outlined),
+              TextField(
+                obscureText: isInvisible,
+                // obscuringCharacter: "x",
+                decoration: InputDecoration(
+                  hintText: "Contraseña",
+                  suffix: IconButton(
+                    onPressed: () {
+                      isInvisible = !isInvisible;
+                      setState(() {
+                        
+                      });
+                    },
+                    icon: isInvisible ? Icon(Icons.remove_red_eye) : Icon(Icons.remove_red_eye_outlined),
+                  ),
                 ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 15,
+              ),
+              TextField(
+                style: GoogleFonts.poppins(),
+                decoration: InputDecoration(
+                  counterText: "",
+                ),
+                onTap: () {
+                  print("On tap");
+                },
+                onChanged: (String text) {
+                  print(text);
+                },
+                maxLength: 8,
+                keyboardType: TextInputType.text,
+                cursorColor: Colors.pinkAccent,
+                readOnly: false,
+                onSubmitted: (String value) {
+                  //Se llama cuando se hace clic en el boton check del telefono, que es el boton de confirmado para que se oculte el teclado
+                  print(value);
+                },
+              ),
+      
+            ],
+          ),
         ),
       ),
     );
